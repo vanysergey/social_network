7700ffc68fac8ec9578a660d3c8133687f5868c8
Sergey Vaniukou <vanysergey89@gmail.com>
2022-05-28 21:27:09 +0300

    Add MessageReducer

---
+++ src/redux/MessageReducer.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {ActionsTypes, dialogsPageType, messagesDataType} from './State';
+
+const ADD_MESSAGE = 'ADD-MESSAGE'
+const UPDATE_NEW_MESSAGE = 'UPDATE-NEW-MESSAGE'
+
+const MessageReducer = (State: dialogsPageType, action: ActionsTypes) => {
+    switch (action.type) {
+        case ADD_MESSAGE:
+            let newMessage: messagesDataType = {
+                id: new Date().getTime(), message: State.newMessageText
+            };
+            State.messagesData.push(newMessage);
+            State.newMessageText = ('');
+            return State;
+        case UPDATE_NEW_MESSAGE:
+            State.newMessageText = action.newMessage;
+            return State;
+        default:
+            return State;
+    }
+
+};
+
+export default MessageReducer;
