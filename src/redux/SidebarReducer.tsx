f17a76627924d33b3ef8e136ce68f74655e6f97b
Sergey Vaniukou <vanysergey89@gmail.com>
2022-05-28 21:28:14 +0300

    Add SidebarReducer

---
+++ src/redux/SidebarReducer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {ActionsTypes, sidebarType} from './State';
+
+const SidebarReducer = (State: sidebarType, action: ActionsTypes) => {
+
+    return State
+};
+
+export default SidebarReducer;
