f08b095ed56619bcadba5ff29b7d7979db46d657
Sergey Vaniukou <vanysergey89@gmail.com>
2022-06-01 17:07:15 +0300

    Add redux-store.tsx

---
+++ src/redux/redux-store.tsx
@@ -0,0 +1,16 @@
+import {combineReducers, legacy_createStore} from 'redux';
+import ProfileReducer from './ProfileReducer';
+import DialogsReducer from './DialogsReducer';
+import SidebarReducer from './SidebarReducer';
+
+// export type ReducesType = ReturnType<typeof reduces>
+
+let reduces = combineReducers({
+    profilePage: ProfileReducer,
+    dialogsPage: DialogsReducer,
+    sidebar: SidebarReducer
+})
+export let store = legacy_createStore(reduces)
+
+
+// export default Store;
