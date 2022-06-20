33cbb2f62fd9826f80d4d482a2b6cf68c3273bc1
Sergey Vaniukou <vanysergey89@gmail.com>
2022-06-20 16:18:23 +0300

    add typification

--- src/redux/ProfileReducer.tsx
+++ src/redux/ProfileReducer.tsx
@@ -1,18 +1,27 @@
-import {ActionsTypes, postsDataType, profilePageType} from './State';
+import {ActionsTypes} from './State';
 
 const ADD_POST = 'ADD-POST'
 const UPDATE_NEW_POST = 'UPDATE-NEW-POST'
+
+type postsDataType = {
+    id: number
+    message: string
+    like: number
+    dislike: number
+}
+
+export type ProfileReducerType = typeof initialState
 
 let initialState = {
     postsData: [
         {id: 1, message: 'Hi, how are you?', like: 35, dislike: 3},
         {id: 2, message: 'It\'s my first post', like: 30, dislike: 2},
         {id: 3, message: 'It\'s my second post', like: 40, dislike: 1},
-    ],
+    ] as Array<postsDataType>,
     newPostText: ''
 }
 
-const ProfileReducer = (State: profilePageType = initialState, action: ActionsTypes) => {
+const ProfileReducer = (State: ProfileReducerType = initialState, action: ActionsTypes): ProfileReducerType => {
     switch (action.type) {
         case ADD_POST:
             const newPost: postsDataType = {
@@ -36,14 +45,15 @@ const ProfileReducer = (State: profilePageType = initialState, action: ActionsTy
 
 export const AddPostAC = () => {
     return {
-        type: 'ADD-POST'
+        type: ADD_POST
     } as const
 }
 export const UpdateNewPostTextActionAC = (text: string) => {
     return {
-        type: 'UPDATE-NEW-POST',
+        type: UPDATE_NEW_POST,
         newText: text
     } as const
 }
+
 
 export default ProfileReducer;
